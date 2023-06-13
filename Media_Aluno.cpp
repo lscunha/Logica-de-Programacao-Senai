30762caec9216771f991c25faa114e50f200bc47
Lucas Santos Cunha <54358807+lscunha@users.noreply.github.com>
2023-06-13 20:07:53 -0300

    Add files via upload

---
+++ Media_Aluno.cpp
@@ -0,0 +1,33 @@
+
+
+
+//Se a not
+#include<stdio.h>
+
+main(){
+	
+
+float nota, notarecuperacao;
+
+printf ("Digite sua nota.");
+scanf("%f", &nota);
+
+if(nota>=7){
+	printf("Aluno Aprovado.");
+} 
+else{
+	printf("Aluno em recuperação.");
+	printf("Digite a nota da recuperacao:");
+    scanf("%f", &notarecuperacao);
+
+      if(notarecuperacao>=5){
+	    printf("Aluno Aprovado pela recuperação.");
+       }else{
+	     printf("Aluno perdeu de ano.");
+       }
+    }
+
+
+}
+
+
